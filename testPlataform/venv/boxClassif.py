575af09fb35d786f551a4484e22f9ad005afcae3
mcdiaz <mcarolinadiaz0@gmail.com>
2019-12-16 13:20:02 -0300

    testPlataform :car:

---
+++ testPlataform/venv/boxClassif.py
@@ -0,0 +1,8 @@
+class boxClassif:
+    def __init__(self, classif, pred, x, y ,h, w):
+        self.c=classif
+        self.p=pred
+        self.x=x
+        self.y=y
+        self.h=h
+        self.w=w
